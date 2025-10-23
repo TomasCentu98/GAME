c6e5a66bfaf9649779561b0445bb88236b69246c
Sebastian Gomez <gomezsebastianemanuel1999@gmail.com>
2025-10-23 19:52:41 -0300

    Creacion de archivos de NPC

--- Includes/NPC_aux.h
+++ Includes/NPC_aux.h
@@ -3,5 +3,20 @@
 class NPC_aux {
 public:
     NPC_aux();
+    void setVida(int);
+    void setFuerza(int);
+    int getVida();
+    int getFuerza();
+    void setNombre(const char *);
+    void setDialogo(const char *);
+    char * getNombre();
+    char * getDialogo();
+    void setSprite(const char *);
+    char * getSprite();
 private:
+    char _dialogo[200] = {};
+    char _nombre[50] = {};
+    char _sprite[100] = {};
+    int _vida;
+    int _fuerza;
 };
--- Sources/NPC_aux.cpp
+++ Sources/NPC_aux.cpp
@@ -1,6 +1,45 @@
 #include "NPC_aux.h"
+#include <cstring>
 
 NPC_aux::NPC_aux() {
-
-
 };
+
+void NPC_aux::setVida(int vida){
+    _vida = vida;
+};
+
+void NPC_aux::setFuerza(int fuerza){
+    _fuerza = fuerza;
+};
+
+int NPC_aux::getFuerza(){
+    return _fuerza;
+};
+
+int NPC_aux::getVida(){
+    return _vida;
+};
+
+void NPC_aux::setNombre(const char *nombre){
+    strcpy(_nombre, nombre);
+};
+
+void NPC_aux::setDialogo(const char *dialogo){
+    strcpy(_dialogo, dialogo);
+};
+
+char* NPC_aux::getNombre(){
+    return _nombre;
+};
+
+char* NPC_aux::getDialogo(){
+    return _dialogo;
+};
+
+void NPC_aux::setSprite(const char *sprite){
+    strcpy(_sprite, sprite);
+};
+
+char* NPC_aux::getSprite(){
+    return _sprite;
+}
